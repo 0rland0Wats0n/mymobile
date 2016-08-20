6d82427d27a63e3c62867743588f1e43f1bbfc96
Orlando Watson <ogw@orlandogwatson.com>
2016-08-19 20:33:32 -0400

    updates

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 
+export const GET_IMAGES = 'GET_IMAGES';
+
+const ROOT_URL = process.env.ROOT_URL || 'http://localhost:4242';
+
+export function getImages() {
+  const request = axios.get(`${ROOT_URL}/api/images`);
+
+  return {
+    type: GET_IMAGES,
+    payload: request
+  };
+};
--- app/main.js
+++ app/main.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, hashHistory } from 'react-router';
+import promise from 'redux-promise';
 
 import routes from './routes';
 import reducers from './reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
