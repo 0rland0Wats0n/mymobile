8903bcbaf017e34d5214e9f2f29f217bd2d18b09
Orlando Watson <ogw@orlandogwatson.com>
2016-08-21 14:11:16 -0400

    resize image on load and resize

--- app/components/photo/index.js
+++ app/components/photo/index.js
@@ -1,20 +1,32 @@
 require('./photo.css');
 
 import _ from 'underscore';
+import $ from 'jquery';
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Footer from '../common-ui/footer/index';
 import { getImage } from '../../actions/index';
 
 class Photo extends Component {
   componentWillMount() {
     this.props.getImage(this.props.params.id);
   };
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+  componentWillUnount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+    $('.mymobile-photo--image').addClass('is-hidden');
+  }
   render() {
     return (
-      <div className="mymobile-photo--container">
-        {this.renderImage()}
+      <div>
+        <div className="mymobile-photo--container">
+          {this.renderImage()}
+        </div>
+        <Footer />
       </div>
     )
   };
@@ -23,7 +35,9 @@ class Photo extends Component {
 
     if(!_.isEmpty(image)) {
       return (
-        <img className="mymobile-photo--image" src={image.image.image.url} />
+        <div className="mymobile-photo--image-container">
+          <img className="mymobile-photo--image is-hidden" src={image.image.image.url} onLoad={this.handleImageLoad} />
+        </div>
       )
     }
 
@@ -31,6 +45,22 @@ class Photo extends Component {
       <p>Loading...</p>
     )
   };
+  handleImageLoad = () => {
+    this.handleWindowResize()
+    $('.mymobile-photo--image').removeClass('is-hidden');
+  };
+  handleWindowResize = () => {
+    const height = $(window).height();
+    const width = $(window).width();
+    const imageWidth = this.props.image.image.image.width;
+    const imageHeight = this.props.image.image.image.height;
+
+    if(imageWidth >= imageHeight) {
+      $(".mymobile-photo--image").css({"width": `${width}px`, "height": "auto", "max-width": "100%"});
+    } else {
+      $(".mymobile-photo--image").css({"height": `${height - 42}px`, "width": "auto"});
+    }
+  }
 }
 
 function mapStateToProps(state) {
